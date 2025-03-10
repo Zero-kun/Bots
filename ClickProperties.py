55eae000495f526ab8017f3a6cd41355c0053a52
Zero-kun <39981888+Zero-kun@users.noreply.github.com>
2025-03-10 12:14:08 -0300

    Update ClickProperties.py

--- ClickProperties.py
+++ ClickProperties.py
@@ -1,24 +1,62 @@
-import win32api
+import win32api, win32con
 import time
+import keyboard
+import pyautogui
 
-width = win32api.GetSystemMetrics(0)
-height = win32api.GetSystemMetrics(1)
-midWidth = int((width + 1) / 2)
-midHeight = int((height + 1) / 2)
+atualized_state = 0
+counter = 0
 
-state_left = win32api.GetKeyState(0x01)  # Left button up = 0 or 1. Button down = -127 or -128
-state_right = win32api.GetKeyState(0x02)  # Right button up = 0 or 1. Button down = -127 or -128
+def GetMultipleStates():
+    left_press = win32api.GetKeyState(0x0001)   #Left Button == 0x001    State == -127 or -128 when pressed and State == 0 or 1 when released
+    right_press = win32api.GetKeyState(0x0002)  #Right Button == 0x002    State == -127 or -128 when pressed and State == 0 or 1 when released
+    wheel_press = win32api.GetKeyState(0x0002)  #Wheel Button == 0x004    State == -127 or -128 when pressed and State == 0 or 1 when released
+    return left_press
     
 while keyboard.is_pressed('q') == False:
-    a = win32api.GetKeyState(0x01)
-    b = win32api.GetKeyState(0x02)
-        
-    if b != state_right:  # Button state changed
-        state_right = b
-        print(b)
-        
-        if b < 0:
-            print('Right Button Pressed')
-        else:
-            print('Right Button Released')
-            win32api.SetCursorPos((midWidth, 50))
+    time.sleep(0.1)
+
+    GetMultipleStates()
+    if keyboard.is_pressed('c') == True:
+        if counter >= 2:
+            win32api.SetCursorPos( (x_inicial, y_inicial) )
+        counter -= 3
+    elif counter <= 0:
+        print('Selecione dois cantos opostos:')
+        counter = 1
+    GetMultipleStates()
+    elif ( left_press < 0 and left_press != atualized_state and counter == 1 ):
+        atualized_state = left_press
+        counter = 2
+        x_inicial, y_inicial = win32api.GetCursorPos()
+        print('Coordenadas selecionadas:    Xi = ', x_inicial, 'Yi = ', y_inicial)        
+        print('Caso tenha selecionado errado, aperte [C]orrigir.\n')
+    elif ( counter == 2):
+        print('Selecione o canto inferior direito:')
+        counter = 3
+    elif ( left_press < 0 and left_press != atualized_state and counter == 3 ):
+        counter = 4
+        x_final, y_final = win32api.GetCursorPos()
+        print('Coordenadas selecionadas:    Xf = ', x_final, 'Yf = ', y_final)
+        print('Caso tenha selecionado errado, aperte [C]orrigir.\n')
+    elif counter == 4:
+        counter = 5
+        delta_x = abs(x_final - x_inicial)
+        delta_y = abs(y_final - y_inicial)
+        x_inicial = min(x_final, x_inicial)
+        y_inicial = min(y_final, y_inicial)
+        im1 = pyautogui.screenshot(region=(x_inicial, y_inicial, delta_x, delta_y))
+        im1.save(r'./savedimage.png')
+        SavedPositions = open('SavedPositions.txt', 'w')
+        text = ('Xi =' + chr(x_inicial) + 'Yi =' + chr(y_inicial) + 'Xf =' + chr(x_inicial + delta_x) + 'Yf =' + chr(y_inicial + delta_y))
+    
+        SavedPositions.write(text)
+        SavedPositions.close()
+        print('Imagem [',delta_x, 'x', delta_y, '] salva.')
+        print('Xi =', x_inicial, 'Yi =', y_inicial, 'Xf =', (x_inicial + delta_x), 'Yf =', (y_inicial + delta_y))
+        print('Caso tenha selecionado errado, aperte [C]orrigir.\n')
+    
+##pyautogui.displayMousePosition()
+##X:  506 Y:  412 RGB: (  0,  34,  56)
+##X: 1398 Y:  946 RGB: (  0,  34,  56)
+
+
